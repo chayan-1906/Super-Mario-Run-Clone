c175137419538f53219ee6d44848ff9ddf0327bf
padma-2021 <chayan19062000@gmail.com>
2021-05-21 02:09:06 +0530

    Initial commit

---
+++ html/src/com/supermariorunclone/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.supermariorunclone.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.supermariorunclone.game.CoinMan;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CoinMan();
+        }
+}
